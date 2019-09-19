b8c77e108279b65d490784820f5b0bc389532bdf
Vvancz <951745827@qq.com>
2019-09-19 14:26:17 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-      我是主页
-  </div>
+  <!-- 先定义一个大容器 -->
+  <el-container>
+    <!-- 先放置一个左侧容器 -->
+    <el-aside></el-aside>
+    <el-container>
+      <!-- 再放置一个右侧容器 -->
+      <!-- 头部 -->
+      <el-header></el-header>
+      <!-- 中部 -->
+      <el-main>
+        <!-- 中部里面放二级路由容器 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
